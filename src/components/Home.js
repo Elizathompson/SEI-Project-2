8ef76df4265152fddde5fa8ea36b2b8c681d1f32
Eliza Thompson <Eliza_thompson@icloud.com>
2020-08-07 15:00:44 +0100

    finsihed game fixed typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => (
         <h1>Trump vs. Kanye</h1>
       </div>
       <div className="home-text">
-        <p>Trump and Kanye are notorious for having big mouths, so we thought it would be good to see how easy it would be to differentiate between these two ‘great minds’. Hit ‘Start’ to see how you get out of 10.</p>
+        <p>Trump and Kanye are notorious for having big mouths, so we thought it would be good to see how easy it would be to differentiate between these two ‘great minds’. Hit ‘Start’ to see how many you get out of 10.</p>
         <p><span className="disclaimer">Disclaimer:</span> These quotes are offensive.</p>
         <p>Lots of Love,<br />Dicktatorship.</p>
       </div>
